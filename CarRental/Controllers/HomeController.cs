c94c8b19e9f291e239470b63324fe3f1e7434c7e
Raghavendra Harish <rh2551@nyu.edu>
2017-05-24 23:56:57 -0400

    Error handling implemented

--- CarRental/Controllers/HomeController.cs
+++ CarRental/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace CarRental.Controllers
 
         public ActionResult About()
         {
+            throw new Exception();
             ViewBag.Message = "Your application description page.";
 
             return View();
